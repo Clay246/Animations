d8a3759b6567e8323e33b056cbdf9b9614a143d9
Clay246 <63927010+Clay246@users.noreply.github.com>
2020-08-02 12:08:41 -0400

    Update mplanimations.py
    Added access to time and eliminated some redundancy

--- mplanimations.py
+++ mplanimations.py
@@ -4,6 +4,9 @@ import numpy as np
 interval = 20
 n = 1000/interval
 
+def time(counter):
+    return (interval*counter)/1000
+
 def sine(initial, final, counter):
     return (initial*(n-n*(.5*np.sin((np.pi/n)*counter-(np.pi/2))+.5))+final*(n*(.5*np.sin((np.pi/n)*counter-(np.pi/2))+.5)))/n
     
@@ -33,12 +36,14 @@ class Transitions:
         """
         Creates a transition between two different alpha values.
         """
+
+        t = time(i)
         
         if transition_type == 'sine':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.alpha_transition_c += 1/transition_time
                 transitionline.set_alpha(sine(alpha1, alpha2, self.alpha_transition_c))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                 self.alpha_transition_c = 0
         
             
@@ -47,21 +52,23 @@ class Transitions:
         """
         Creates a transition between axis limits.
         """
+
+        t = time(i)
         
         if transition_type == 'sine':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.axis_transition_c += 1/transition_time
                 axis.set_xlim(sine(xlim1[0], xlim2[0], self.axis_transition_c), sine(xlim1[1], xlim2[1], self.axis_transition_c))
                 axis.set_ylim(sine(ylim1[0], ylim2[0], self.axis_transition_c), sine(ylim1[1], ylim2[1], self.axis_transition_c))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                     self.axis_transition_c = 0
                     
         if transition_type == 'linear':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.axis_transition_c += 1/transition_time
                 axis.set_xlim(linear(xlim1[0], xlim2[0], self.axis_transition_c), sine(xlim1[1], xlim2[1], self.axis_transition_c))
                 axis.set_ylim(linear(ylim1[0], ylim2[0], self.axis_transition_c), sine(xlim1[1], xlim2[1], self.axis_transition_c))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                     self.axis_transition_c = 0
                     
                     
@@ -70,19 +77,21 @@ class Transitions:
         """
         Creates a color transition for using the specified color map and the initial and final colors.
         """
+
+        t = time(i)
     
         if transition_type == 'sine':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.color_transition_c += 1/transition_time
                 transitionline.set_color(cmap(sine(c1, c2, self.color_transition_c)))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                 self.color_transition_c = 0
                 
         if transition_type == 'linear':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.color_transition_c += 1/transition_time
                 transitionline.set_color(cmap(linear(c1, c2, self.color_transition_c)))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                 self.color_transition_c = 0
                     
                     
@@ -91,37 +100,39 @@ class Transitions:
         """
         Creates a transition between dot sizes for all dots in a scatter plot.
         """
+
+        t = time(i)
         
         if transition_type == 'sine':
             if duration == 'inf':
-                if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+                if t >= starttime and t < starttime + transition_time:
                     self.dot_transition_c += 1/transition_time
                     dot.set_sizes([sine(size1, size2, self.dot_transition_c)])
-                elif (interval*i)/1000 == starttime + transition_time:
+                elif t == starttime + transition_time:
                     self.add_dot_c = 0
             else:
-                if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+                if t >= starttime and t < starttime + transition_time:
                     self.dot_transition_c += 1/transition_time
                     dot.set_sizes([sine(size1, size2, self.dot_transition_c)])
-                elif (interval*i)/1000 >= starttime + duration and (interval*i)/1000 < starttime + duration + transition_time:
+                elif t >= starttime + duration and t < starttime + duration + transition_time:
                     self.dot_transition_c += 1/transition_time
                     dot.set_sizes([sine(size1, size2, self.dot_transition_c)])
                     
         if transition_type == 'linear':
             if duration == 'inf':
-                if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+                if t >= starttime and t < starttime + transition_time:
                     self.dot_transition_c += 1/transition_time
                     dot.set_sizes([linear(size1, size2, self.dot_transition_c)])
-                elif (interval*i)/1000 == starttime + transition_time:
+                elif t == starttime + transition_time:
                     self.add_dot_c = 0
             else:
-                if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+                if t >= starttime and t < starttime + transition_time:
                     self.dot_transition_c += 1/transition_time
                     dot.set_sizes([linear(size1, size2, self.dot_transition_c)])
-                elif (interval*i)/1000 == starttime + transition_time:
+                elif t == starttime + transition_time:
                     # This is required for the linear function because it is not periodic and the dot transition will occur again at the end.
                     self.dot_transition_c=0
-                elif (interval*i)/1000 >= starttime + duration and (interval*i)/1000 < starttime + duration + transition_time:
+                elif t >= starttime + duration and t < starttime + duration + transition_time:
                     self.dot_transition_c += 1/transition_time
                     dot.set_sizes([linear(size2, size1, self.dot_transition_c)])
             
@@ -131,19 +142,21 @@ class Transitions:
         """
         Creates a transition between two functions of x
         """
+
+        t= time(i)
         
         if transition_type == 'sine':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.line_transition_c += 1/transition_time
                 transitionline.set_ydata(sine(yvals1, yvals2, self.line_transition_c))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                 line_transition_c = 0
                 
         if transition_type == 'linear':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.par_transition_c += 1/transition_time
                 transitionline.set_ydata(linear(yvals1, yvals2, self.line_transition_c))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                 self.par_transition_c = 0
                 
                 
@@ -153,19 +166,21 @@ class Transitions:
         Creates a transition between two sets of parametric equations.
         This function is capable of doing the same thing as line_transition(), but requires two extra arguments.
         """
+
+        t = time(i)
         
         if transition_type == 'sine':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.par_transition_c += 1/transition_time
                 transitionline.set_data(sine(xvals1, xvals2, self.par_transition_c), sine(yvals1, yvals2, self.par_transition_c))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                 self.par_transition_c = 0
                 
         if transition_type == 'linear':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.par_transition_c += 1/transition_time
                 transitionline.set_data(linear(xvals1, xvals2, self.par_transition_c), linear(yvals1, yvals2, self.par_transition_c))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                 self.par_transition_c = 0
 
 
@@ -175,20 +190,21 @@ class Transitions:
         Creates a variable that transitions between two values.
         """
 
+        t = time(i)
         self.var = initial
         
         if transition_type == 'sine':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.var_transition_c += 1/transition_time
                 self.var = sine(initial, final, self.var_transition_c)
                 transitionline.set_ydata(func(self.var))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                 self.par_transition_c = 0
 
         if transition_type == 'linear':
-            if (interval*i)/1000 >= starttime and (interval*i)/1000 < starttime + transition_time:
+            if t >= starttime and t < starttime + transition_time:
                 self.var_transition_c += 1/transition_time
                 self.var = linear(initial, final, self.var_transition_c)
                 transitionline.set_ydata(func(self.var))
-            elif (interval*i)/1000 == starttime + transition_time:
+            elif t == starttime + transition_time:
                 self.par_transition_c = 0
