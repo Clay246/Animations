244f778321cb8eba65de3fa2de8bdfcf5f6a6e73
Clay246 <63927010+Clay246@users.noreply.github.com>
2020-08-03 17:21:08 -0400

    Update HuygensPrinciple.py
    Updated code for new version of mplanimations

--- HuygensPrinciple/HuygensPrinciple.py
+++ HuygensPrinciple/HuygensPrinciple.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import mplanimations
+import mplanimations as ans
 
 t = np.linspace(0, 2*np.pi, 100)
 
@@ -23,7 +23,7 @@ lines = [ax1.plot(t*0, t*0, linewidth=2, c='white', zorder=1, alpha=.5)[0] for i
 
 dots = [ax1.scatter(np.cos(i*np.pi/4), np.sin(i*np.pi/4), color='white', s=0) for i in range(8)]
 
-ts = [mplanimations.Transitions() for i in range(29)]
+ts = [ans.Transitions() for i in range(29)]
 
 r = 12
 ttime = 11.5
@@ -33,19 +33,19 @@ dur = 12.7
 def animate(i):
 
     ts[0].dot_transition(i, 1, 0, ds, tdot1, duration=17.5)
-    ts[1].par_transition(i, 2, t*0, np.cos(t), t*0, np.sin(t), tline1, transition_type='sine')
+    ts[1].par_transition(i, 2, t*0, np.cos(t), t*0, np.sin(t), tline1, transition_type=ans.sine)
     
     for n in range(8):
-        ts[2+n].dot_transition(i, 3+n/10, 0, ds, dots[n], duration=dur, transition_type='sine')
+        ts[2+n].dot_transition(i, 3+n/10, 0, ds, dots[n], duration=dur, transition_type=ans.sine)
     
     for n in range(8):
-        ts[10+n].par_transition(i, 4.2, half_circles[n]*0+np.cos(n*np.pi/4), r*np.cos(half_circles[n])+np.cos(n*np.pi/4), half_circles[n]*0+np.sin(n*np.pi/4), r*np.sin(half_circles[n])+np.sin(n*np.pi/4), lines[n], transition_time=ttime, transition_type='linear')
+        ts[10+n].par_transition(i, 4.2, half_circles[n]*0+np.cos(n*np.pi/4), r*np.cos(half_circles[n])+np.cos(n*np.pi/4), half_circles[n]*0+np.sin(n*np.pi/4), r*np.sin(half_circles[n])+np.sin(n*np.pi/4), lines[n], transition_time=ttime, transition_type=ans.linear)
     
     ts[18].axis_transition(i, 5, [-4, 4], [-7.5, 7.5], [-4, 4], [-7.5, 7.5], ax1)
-    ts[19].par_transition(i, 17.5, np.cos(t), t*0, np.sin(t), t*0, tline1, transition_type='sine')
+    ts[19].par_transition(i, 17.5, np.cos(t), t*0, np.sin(t), t*0, tline1, transition_type=ans.sine)
     
     for n in range(8):
-        ts[20+n].color_transition(i, 4, plt.cm.binary, 0, 1, lines[n], transition_time=ttime, transition_type='linear')
+        ts[20+n].color_transition(i, 4, plt.cm.binary, 0, 1, lines[n], transition_time=ttime, transition_type=ans.linear)
     
     
 ani = animation.FuncAnimation(fig, animate, interval=20, frames=975, repeat=False)
